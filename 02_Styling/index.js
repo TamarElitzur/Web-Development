d4ba62756bc3ba379cf7e15c0f71958994cf5df3
Tamar <tamarbirnbaum@gmail.com>
2025-11-18 18:22:11 +0200

    lesson 4

---
+++ 02_Styling/index.js
@@ -0,0 +1,8 @@
+let x = 0;
+// This function increments x and logs/alerts the current value
+function DoSomething()
+{
+    x++;
+    console.log("Current number is:"+x);
+    alert(`Current number is ${x} with alert`);
+}
